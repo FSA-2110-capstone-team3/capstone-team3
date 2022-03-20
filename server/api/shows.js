bb1a34ef55249a9afb129665413463da207e96e1
Jay Flanary <j.c.flanary@gmail.com>
2022-03-20 12:26:31 -0400

    added spotify routes

--- server/api/shows.js
+++ server/api/shows.js
@@ -4,8 +4,13 @@ const {
 } = require("../db");
 const queryString = require("query-string");
 const axios = require("axios");
+const { spotifyApi } = require('../app');
 
 module.exports = router;
+
+
+
+/*<------DB Model Routes-------->*/
 
 
 // GET all shows from db
@@ -79,26 +84,16 @@ router.get('/:id', async (req, res, next) => {
 
 
 
-// Using Joe Rogan show id to test
-// Remember to replace the token below with YOURS to test
-router.get("/4rOoJ6Egrf8K2IrywzwOMk", async (req, res, next) => {
+/*<------Spotify API Routes-------->*/
+
+
+//GET show
+router.get('/spotify/:id', async(req, res, next) => {
   try {
-    const shows = (
-      await axios.get(
-        `https://api.spotify.com/v1/shows/4rOoJ6Egrf8K2IrywzwOMk`,
-        {
-          headers: {
-            Accept: "application/json",
-            Authorization:
-              "Bearer " +
-              "BQAkikuAsFwHHymkXOM72zpid0IgByom_VSJ3P_izTWHUBRoYEzU2YM1RJ0pliPipx9riuJDHQEq54zE-1kirUmWKnBOUFaNHI2Lj5xQNXSBP6MjlVw2VvXcNONrFqJ345Mo76ae6g3QRAl63kow8cL2Sls4oJedbx8",
-            "Content-Type": "application/json",
-          },
-        }
-      )
-    ).data;
-    res.json(shows);
-  } catch (err) {
-    next(err);
+    const showId = req.params.id;
+    const response = await spotifyApi.getShow(showId,{market: 'US'});
+    res.send(response);
+  } catch(ex) {
+    next(ex);
   }
 });
