fdbb9d703b2c60e6932bb864faeab5580aa0ebfa
Jay F <85573433+jayflan@users.noreply.github.com>
2022-03-21 14:24:47 -0400

    Update db.js
    Removing my pg password, as that's just for me so pg works on my machine.

--- server/db/db.js
+++ server/db/db.js
@@ -22,7 +22,7 @@ if (process.env.DATABASE_URL) {
 }
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://postgres:Bigman<26@localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL || `postgres//localhost:5432/${databaseName}`,
   config
 );
 module.exports = db;
