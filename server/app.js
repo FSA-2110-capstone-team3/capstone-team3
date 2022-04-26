fe36a51105fdb0831a1a0644ddd2b163ae79360e
David Woo <dwoo1016@gmail.com>
2022-04-25 23:01:16 -0400

    added guest credential functionality

--- server/app.js
+++ server/app.js
@@ -5,18 +5,11 @@ const app = express();
 let request = require("request");
 let querystring = require("querystring");
 const qs = require("qs");
-// const env = require(".././.env");
 require("dotenv").config();
 const axios = require("axios");
 const User = require("./db/models/User");
 const SpotifyWebApi = require("spotify-web-api-node");
 
-// process.env.SPOTIFY_CLIENT_ID = env.SPOTIFY_CLIENT_ID;
-// process.env.SPOTIFY_CLIENT_SECRET = env.SPOTIFY_SECRET_KEY;
-// process.env.REDIRECT_URI = env.REDIRECT_URI;
-
-// Create the api library object with the credentials
-//Spotify 'client-credential-flow' === 'https://developer.spotify.com/documentation/general/guides/authorization/client-credentials/'
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_SECRET_KEY,
@@ -26,37 +19,26 @@ module.exports = {
   app,
   spotifyApi,
 };
-// console.log(process.env.SPOTIFY_CLIENT_ID);
-// console.log(process.env.SPOTIFY_CLIENT_SECRET);
-// console.log(process.env.REDIRECT_URI);
 
-//----------- TRYING OAUTH
-//Used https://github.com/mpj/oauth-bridge-template spotify OAUTH template and filled it in with our localhost
-
-let redirect_uri = process.env.REDIRECT_URI || "http://localhost:8080/callback"; //!We need to tell dev spotify sit that this callback URI is valid for security purposes. Needs to be the same URI as the one on dev spotify
-
+let redirect_uri = process.env.REDIRECT_URI || "http://localhost:8080/callback"; 
 app.get("/login", function (req, res) {
   //! serves /login
   res.redirect(
     //! immediatelyy re-directs
-    "https://accounts.spotify.com/authorize?" + //! with all these querystrings
+    "https://accounts.spotify.com/authorize?" + 
       querystring.stringify({
-        //! not sure why its decprecated, still works tho
         response_type: "code",
         client_id: process.env.SPOTIFY_CLIENT_ID,
         scope:
           "user-read-private user-read-email user-library-read user-library-modify",
-        redirect_uri: redirect_uri, //!then redirects to our localhost declared above
+        redirect_uri: redirect_uri, 
       })
   );
 });
 
 app.get("/callback", async function (req, res) {
-  //!the one thats receiving the redirect back from spotify. Which receives a code and then uses along with a secret to grab the access token. Then takes the access token and sends it off to our front end.
   try {
-    // console.log('REQ QUERYCODE', req.query.code)
     let code = req.query.code || null;
-
     axios({
       method: "post",
       url: "https://accounts.spotify.com/api/token",
@@ -76,7 +58,6 @@ app.get("/callback", async function (req, res) {
         if (response.status === 200) {
           const ACCESS_TOKEN = response.data.access_token;
           const { access_token, refresh_token, token_type } = response.data;
-          // access_token = response.data.access_token
           axios
             .get("https://api.spotify.com/v1/me", {
               headers: {
@@ -116,52 +97,9 @@ app.get("/callback", async function (req, res) {
       .catch((error) => {
         res.send(error);
       });
-    // console.log('EMAIL', email)
-    // console.log('ACCESS TOKEN FROM AXIOS POST', ACCESS_TOKEN)
-
-    // const response = (await axios.post())
-
-    // request.post(authOptions, function (error, response, body) {
-    //   access_token = body.access_token;
-    //   // let uri = process.env.FRONTEND_URI || "http://localhost:8080/";
-    //   console.log('ACCESS TOKEN--->', access_token)
-    // });
-
-    // const token = await User.authenticate(userInfo);
-    // console.log('MY TOKEN--->', token);
-    // res.send(`
-    //   <html>
-    //     <body>
-    //       <script>
-    //         window.localStorage.setItem('token', '${token}');
-    //         window.document.location = '/';
-    //       </script>
-    //     </body>
-    //   </html>
-    // `);
   } catch (ex) {
     console.log(ex);
   }
-
-  // console.log('req', req.query.code);
-
-  // request.post(authOptions, async function (error, response, body) {
-  //   const access_token = body.access_token;
-  //   let uri = process.env.FRONTEND_URI || "http://localhost:8080/";
-  //   //!once we log into spotify and have access to Spotify's data, our backend send you back to the front-end, (aka to the link above) so back to our react app
-  //   //!where our react app is hosted in production (once we deploy on heroku, itll be out heroku link)
-  //   res.redirect(uri + "?access_token=" + access_token);
-  //   // const user = (await axios.get('https://api.spotify.com/v1/me',
-  //   // {
-  //   //   headers: {
-  //   //     Accept: "application/json",
-  //   //     Authorization: "Bearer " + access_token,
-  //   //     "Content-Type": "application/json",
-  //   //   },
-  //   // }
-  //   // )).data;
-  //   // console.log(user);
-  // });
 });
 
 //Refresh user Access Token using the spotify-web-api library & update User db model
